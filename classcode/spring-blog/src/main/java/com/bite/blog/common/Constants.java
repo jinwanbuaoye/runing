d3e24ae1f0da268ed364916f561cdf1162f7dc51
陈凯 <1684145861@qq.com>
2025-04-18 20:15:42 +0800

    1

--- classcode/spring-blog/src/main/java/com/bite/blog/common/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.bite.blog.common;
-
-public class Constants {
-    public static final Integer RESULT_SUCCESS = 200;
-    public static final Integer RESULT_FAIL = -1;
-}
--- classcode/spring-blog/src/main/java/com/bite/blog/utils/DateUtils.java
+++
@@ -1,9 +0,0 @@
-package com.bite.blog.utils;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-public class DateUtils {
-    public static String format(Date date){
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return simpleDateFormat.format(date);
-    }
-}
